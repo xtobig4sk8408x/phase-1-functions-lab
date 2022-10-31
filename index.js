f159b45c1fb25de26d8a69dc74556e38b96dd178
Theodore John Josef <john.josef@aol.com>
2022-10-31 15:41:06 -0700

    commit tree e8fe3e6e8b41767c9f078d0d97caf2c639fd6d26

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(customerLocation) {
+    const hqLocation = 42;
+    var blocksFromHQ;
+    if(customerLocation > hqLocation) {
+        blocksFromHQ = customerLocation - hqLocation; 
+    } else {
+        blocksFromHQ = hqLocation - customerLocation;
+    }
+    return blocksFromHQ;
+}
+
+function distanceFromHqInFeet(customerLocation) {
+    return distanceFromHqInBlocks(customerLocation) * 264;
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (destination > start) {
+        return (destination - start) * 264;
+    } else { 
+        return (start - destination) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+    }  else if (distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance <=2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
